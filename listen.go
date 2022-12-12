53ea8f506979dad7e489b0dd0a1e823a58276b89
master <test3@damianfurrer.ch>
2022-12-12 17:47:50 +0100

    initial

---
+++ listen.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "os"
+	"net"
+	"io"
+)
+
+func echoServer(c net.Conn) {
+    log.Printf("Client connected [%s]", c.RemoteAddr().Network())
+    io.Copy(c, c)
+    c.Close()
+}
+
+func main() {
+    mydir, err := os.Getwd()
+    if err != nil {
+        fmt.Println("Can't get Current Directory",err.Error())
+		return
+    }
+	SockAddr:=mydir + "/echo.sock"
+
+    if err := os.RemoveAll(SockAddr); err != nil {
+        panic(err)
+    }
+
+    l, err := net.Listen("unix", SockAddr)
+    if err != nil {
+        fmt.Println("listen error:",err.Error())
+    }
+    defer l.Close()
+
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("accept error:", err.Error())
+        }
+
+        go echoServer(conn)
+    }
+}
